2f27ce6672ac0a78424513ca645360c8f63a82b0
Ed Locquiao <nilatian.15@gmail.com>
2022-06-27 21:11:26 -0700

    Lab excercise first commit

---
+++ Application1.java
@@ -0,0 +1,24 @@
+
+public class Application1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//First solution to Lab 1: Creating a variable that holds the available quantity of seats left on a plane
+
+		// Providing the value 34 as the available seats
+		int availableSeatsleft = 34;
+
+		//If code runs without error, the integer variable identified as availableSeatsleft should display '34'
+		System.out.println("The quantity of available seats left on the plane is:" + " " + availableSeatsleft + ".");
+
+		// Second solution to Lab 1: Creating a variable that holds the available quantity of seats left on a plane
+
+		// Providing the value 34 as the available seats
+		Boolean testVariable = availableSeatsleft == 34; 
+
+		//Output the result of testVariable. If true, the validity of availableSeatsleft variable proves the code is functioning properly
+		System.out.println(testVariable);
+		
+	}
+
+}
